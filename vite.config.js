1f7a4c62d8bd92820ca7d773133b8ad40b9e57f8
Vladimir Shmidt <vladshmi3@gmail.com>
2025-10-27 14:52:48 +0100

    More

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nombre-de-tu-repositorio/', // ⚠️ Cambia esto por el nombre de tu repo
+  base: 'https://github.com/VladShmi/hack-web',
 })
